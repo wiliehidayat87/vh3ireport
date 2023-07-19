4958b66ffd8a83b7cd0bed728eb764856404a85e
Wilie Wahyu Hidayat <wilie.hidayat87@gmail.com>
2023-07-19 18:08:00 +0700

    fix text for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -151,7 +151,7 @@ export default function Home() {
               <div key={index} className="card m-2 cursor-pointer border border-gray-400 rounded-lg hover:shadow-md hover:border-opacity-0 transform hover:-translate-y-1 transition-all duration-200 bg-white" onClick={(e) => handleClickStat(e, item.Keys, item.Values)}>
                 <div className="m-3">
                   <span className="text-sm text-teal-800 font-mono bg-teal-100 inline rounded-full px-2 align-top float-right animate-pulse">{item.Tag}</span>
-                  <h2 className="text-lg mb-2 font-bold">
+                  <h2 className="text-lg mb-2 font-bold sm:text-black">
                     {
                       item.Keys == "CURREV"
                         ? new Intl.NumberFormat("de-DE", {style: "currency", currency: "IDR"}).format(item.Values)
